234d20a6b80f7555ac95136e41473eb9daa1ed53
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2020-05-24 23:30:53 +0900

    Add css

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Layout from "../../components/layout";
 import Date from "../../components/date";
 import { getAllPostIds, getPostData } from "../../lib/posts";
+import utilStyles from "../../styles/utils.module.css";
 
 export const getStaticPaths = async (): Promise<{
   paths: {
@@ -57,12 +58,13 @@ export default ({
       <Head>
         <title>{postData.title}</title>
       </Head>{" "}
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      <Date dateString={postData.date} />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }}></div>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 };
