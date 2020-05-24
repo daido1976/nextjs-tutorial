c089de0d7dfdeed34ee9f18966475a31c2f3ec11
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2020-05-24 23:20:11 +0900

    Add title to the Post Page

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 
@@ -52,7 +53,9 @@ export default ({
 }): JSX.Element => {
   return (
     <Layout home={false}>
-      {" "}
+      <Head>
+        <title>{postData.title}</title>
+      </Head>{" "}
       {postData.title}
       <br />
       {postData.id}
