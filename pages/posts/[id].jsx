bf6026570a11aea602f3efbc361376491918d14b
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2020-05-24 20:55:05 +0900

    Impl getStaticPaths() in pages/posts/[id].jsx

--- pages/posts/[id].jsx
+++ pages/posts/[id].jsx
@@ -1,4 +1,13 @@
 import Layout from "../../components/layout";
+import { getAllPostIds } from "../../lib/posts";
+
+export const getStaticPaths = async () => {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+};
 
 export default function Post() {
   return <Layout>...</Layout>;
