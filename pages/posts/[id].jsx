6ec8444291cbdfd525aaa49baa99bd16a669590f
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2020-05-24 21:02:36 +0900

    Impl getStaticProps() in pages/posts/[id].jsx

--- pages/posts/[id].jsx
+++ pages/posts/[id].jsx
@@ -1,11 +1,20 @@
 import Layout from "../../components/layout";
-import { getAllPostIds } from "../../lib/posts";
+import { getAllPostIds, getPostData } from "../../lib/posts";
 
 export const getStaticPaths = async () => {
   const paths = getAllPostIds();
   return {
     paths,
     fallback: false,
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const postData = getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
   };
 };
 
